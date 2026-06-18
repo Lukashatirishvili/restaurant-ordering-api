88141438d26d80d0e21ee96740cd2e2117ff3ba2
Luka Shatirishvili <lukkashatirishvili@gmail.com>
2026-06-18 23:36:22 +0400

    first commit

---
+++ RestaurantOrdering.Api/Models/Category.cs
@@ -0,0 +1,12 @@
+namespace RestaurantOrdering.Api.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    public List<MenuItem> MenuItems { get; set; } = new();
+}
---
+++ RestaurantOrdering.Api/Models/MenuItem.cs
@@ -0,0 +1,18 @@
+namespace RestaurantOrdering.Api.Models;
+
+public class MenuItem
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    public decimal Price { get; set; }
+
+    public bool IsAvailable { get; set; } = true;
+
+    public int CategoryId { get; set; }
+
+    public Category Category { get; set; } = null!;
+}
